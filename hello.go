d5eb323ad38fde3ef7ce5275a46dfa16123c3044
Thai Anh Duc <anhduc.thai@gmail.com>
2019-11-26 22:15:47 +0700

    hello Go

---
+++ hello.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	/*
+		There is a strict rule of the main entry. The package name must be "main" so the main function.
+		Compilation error if violated
+
+		The bracket "{" must be in the same line with the function name
+	*/
+	fmt.Println("My very first Go application. Just follow whatever written on the internet")
+}
