40ce40967a459fba12ea73d110bae3a81623264b
Thai Anh Duc <anhduc.thai@gmail.com>
2019-11-27 17:29:04 +0700

    More cool stuff of Go - defer function, pointer

--- hello.go
+++ hello.go
@@ -13,9 +13,7 @@ func main() {
 	*/
 	fmt.Println("My very first Go application. Just follow whatever written on the internet")
 
-	/*
-		This is an integer
-	*/
+	// This is an integer
 	a := 10
 	fmt.Println(a)
 
@@ -30,9 +28,44 @@ func main() {
 		fmt.Println(persons[i])
 	}
 
-	/*
-		Runtime array size
-	*/
+	// Runtime array size
 	tasks := [...]int{1, 2, 3, 4, 5, 6}
 	fmt.Println(len(tasks))
+	fmt.Println("Print an array: ", tasks)
+
+	// Prepare defer functions
+	defer deferCallWorkLikeCallBack()
+
+	// Let's call some user defined functions
+	sum, div := calculate(2, 4)
+	fmt.Println("Call func result: ", sum, div)
+}
+
+func calculate(a int, b int) (int, int) {
+	// Can return multiple values
+	// if the function name starts with Capital letter -> public method
+	// otherwise, it is private
+	// So Calculate will allow other packages to access it
+	return a + b, a - b
+}
+
+func deferCallWorkLikeCallBack() {
+	// Is it possible to pass the returned value from calling method to the deferred function?
+	var number int = 1
+	// This will pass 1 to the deferred method. So not a reference
+	defer deferInDefer(number)
+	// This will pass the latest number, which is 13, to the deferred function
+	// Pass the address (pointer) of the number variable
+	defer deferWithPointer(&number)
+	fmt.Println("Hi I am a deferred call. You can think of me as a callback in your word")
+	number = 13
+}
+
+func deferInDefer(number int) {
+	fmt.Println("Hi you passed the number:", number)
+}
+
+func deferWithPointer(number *int) {
+	// This allows the function to access the latest value passed from the calling function
+	fmt.Println("Hi you passed the pointer to number:", *number)
 }
