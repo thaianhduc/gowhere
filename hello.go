e25b4c69f7c9b3480528970ea68ac064ce8d2a8b
Thai Anh Duc <anhduc.thai@gmail.com>
2019-11-27 11:18:16 +0700

    Go - playing around with declaring type, loop and array

--- hello.go
+++ hello.go
@@ -12,4 +12,27 @@ func main() {
 		The bracket "{" must be in the same line with the function name
 	*/
 	fmt.Println("My very first Go application. Just follow whatever written on the internet")
+
+	/*
+		This is an integer
+	*/
+	a := 10
+	fmt.Println(a)
+
+	persons := [4]string{
+		"Thai Anh Duc",
+		"Dao Dang Khanh Ngoc",
+		"Thai Ngoc Diep",
+		"Thai Diep Chi"}
+
+	var i int
+	for i = 0; i < len(persons); i++ {
+		fmt.Println(persons[i])
+	}
+
+	/*
+		Runtime array size
+	*/
+	tasks := [...]int{1, 2, 3, 4, 5, 6}
+	fmt.Println(len(tasks))
 }
